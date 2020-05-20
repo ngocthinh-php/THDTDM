f327cfaa84c648e54084bf3d102a7ec016007c91
ngocthinh-php <thinhb1609550@student.ctu.edu.vn>
2020-05-20 08:52:45 +0700

    This is first index.php page

---
+++ form.php
@@ -0,0 +1,6 @@
+<?php>
+var $name=$_POST[['fname'];
+var $mail =$_POST['mail'];
+echo "Chao ban $name </br> 
+Email cua ban la: $mail";
+<?php>
---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<form action ="/form.php" method="POST">
+<P>Ho Ten: <input type ="text" name ="fname"></p>
+<p>Email: <input type="text" name="mail"></p>
+<input type ="submit" name="submit">
+</form>
